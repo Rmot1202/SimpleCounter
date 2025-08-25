ec8fc3a1858c654eac95139d58cd36576d2f17fd
Rmot1202 <mottraven43@gmail.com>
2025-08-25 16:27:56 -0400

    gn

--- app/src/main/java/com/example/simplecounter/MainActivity.kt
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -1,20 +1,49 @@
 package com.example.simplecounter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+            Toast.makeText(this, "Clicked Button", Toast.LENGTH_SHORT).show()
+        }
+        val upgradeBtn = findViewById<Button>(R.id.upgradeBtn)
+
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+                // Show the upgrade button and set its click listener
+                upgradeBtn.visibility = View.VISIBLE
+                upgradeBtn.setOnClickListener {
+                    button.text = getString(R.string.plus2)
+
+                    // Update the button click listener to add 2 instead of 1
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+                    // Hide the upgrade button again
+                    upgradeBtn.visibility = View.INVISIBLE
+                    Toast.makeText(this, "Upgrade Button Clicked", Toast.LENGTH_SHORT).show()
+                }
+
+            }
         }
     }
 }
